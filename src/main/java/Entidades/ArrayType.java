ba1df0873e003c0e909f02fce71c3b076acfd796
Rudelvi <rudelvi1096@gmail.com>
2021-03-09 20:16:37 -0400

    Primer Commit;

---
+++ src/main/java/Entidades/ArrayType.java
@@ -0,0 +1,23 @@
+package Entidades;
+
+/**
+ * A type that is an array.
+ */
+public class ArrayType extends Type {
+
+    private Type baseType;
+
+    public ArrayType(Type baseType) {
+        super(baseType.getName() + "[]");
+        this.baseType = baseType;
+    }
+
+    public Type getBaseType() {
+        return baseType;
+    }
+
+    @Override
+    public void analyze(AnalysisContext context) {
+        baseType.analyze(context);
+    }
+}
